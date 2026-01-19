9a062c20916d4ca8753466b39ef22bdb22232945
Mohidul <islamohidul856mi647360@gmail.com>
2026-01-19 20:48:12 +0600

    Updated settings debug

--- blog_post_web/settings.py
+++ blog_post_web/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS=['https://robotics-blog-post-1.onrender.com']
+CSRF_TRUSTED_ORIGINS=['https://robotics-blog-post.onrender.com']
 
 SECRET_KEY=config('SECRET_KEY')
 # Application definition
