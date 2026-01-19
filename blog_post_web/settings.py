73897913946f2f9729f7d209098cca609b2c064f
Mohidul <islamohidul856mi647360@gmail.com>
2026-01-19 20:44:14 +0600

    updated settings

--- blog_post_web/settings.py
+++ blog_post_web/settings.py
@@ -14,8 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS=['https://robotics-blog-post-1.onrender.com']
 
